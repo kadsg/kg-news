d5aed936fd58c52432b8946245053fbcfda52003
MingJie Li <kadsg0769@163.com>
2024-05-04 15:44:08 +0800

    fix: 修复PageHelper分页生效的问题

--- server/src/main/java/kg/news/config/PageHelperConfiguration.java
+++ server/src/main/java/kg/news/config/PageHelperConfiguration.java
@@ -5,12 +5,7 @@ import org.apache.ibatis.plugin.Interceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/**
- * PageHelper 分页配置
- *
- * @author pjy
- * @date 2021年07月29日
- */
+
 @Configuration
 public class PageHelperConfiguration {
 
